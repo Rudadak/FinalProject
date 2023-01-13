f20709f240cece9b2a079f29670b5baf74bfdc1c
repiel <hi27194591@gmail.com>
2023-01-13 12:00:21 +0900

    dd

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,30 @@
-
-import logo from './logo.svg';
+import logo from './회전루피.gif';
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
-
+      <header className="App-header">
+        <a
+          href = "https://mk28.tistory.com/245"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <p>
+          테스트 페이지
+        </p>
+        <a
+          className="App-link"
+          href="https://velog.io/@gym/React-React%EB%A1%9C-%ED%95%98%EB%8B%A8%ED%83%AD-%EB%A7%8C%EB%93%A4%EA%B8%B0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          하단 네비게이션 만들기llldsdfwataw4rㄴㅇㄹㄴㅇㄹㄷㅈㄺㅅㅁㅈㄷㄱㄹ
+          dlkjslkfjldskjlfkdsjlkf
+        </a>
+      </header>
     </div>
   );
 }
