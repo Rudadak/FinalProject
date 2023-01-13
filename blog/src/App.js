5b2197c22e6e841c1b86f585b76edee16c16008e
repiel <hi27194591@gmail.com>
2023-01-13 10:41:55 +0900

    kk

--- blog/src/App.js
+++ blog/src/App.js
@@ -21,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          하단 네비게이션 만들기
+          하단 네비게이션 만들기lll
         </a>
       </header>
     </div>
