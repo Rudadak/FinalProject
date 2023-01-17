2840642484a27720201fa72cfaf2388968b51bbb
Null2648 <jhkim208@naver.com>
2023-01-17 17:44:02 +0900

    전체 저장

--- BackEnd workspace/sample/sample1/src/App.js
+++ BackEnd workspace/sample/sample1/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  let [logo, setLogo] = useState('시각장애인 서비스');
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "black-nav">
+        <h1 style={ {color : 'red', fontSize : '40px'} }>{ logo }</h1>
+        <Link to="/test">
+        <Button type= "button" color="secondary" variant="contained"><h4>로그인</h4></Button>
+      </Link>
+      </div>
+
+        <div>
+        <Routes>
+          <Route path = "/" element={<Home />} />
+        </Routes>
+        </div>
     </div>
+    
+    </BrowserRouter>
   );
 }
 
+function Home(){
+  return(
+    <div className="home">
+      <div calssName='abc'>
+ㅁ
+      </div>
+    </div>
+  )
+}
 export default App;
