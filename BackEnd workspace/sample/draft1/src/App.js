381c32f47897e0ab559b04c72c1894db9e8ae330
Null2648 <jhkim208@naver.com>
2023-01-19 17:33:00 +0900

    페이지 이동 구현한

--- BackEnd workspace/sample/draft1/src/App.js
+++ BackEnd workspace/sample/draft1/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import{BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import ScrollToTop from './components/scroll-to-top/ScrollToTop';
+
+
 
 function App() {
+
+  // let [로그인,로그인변경] = useState('로그인')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        </Routes>
+        </BrowserRouter>
+      </div>
+
+    
+  );
+}
+
+function Home() {
+  return(
+    <div>
+      <Head/>
+      <div>
+      안녕하세요
+      <LoginButton/>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Head(){
+  return(
+    <div>
+    <header>위 페이지는 루다닥에서 제공합니다.</header>
+    </div>
+  );
+
+}
+
+
+function LoginButton(){
+  return(
+    <div>
+    <nav>
+        <Button variant="outlined">
+          <Link to ={'/login'}>로그인</Link><br/>
+        </Button>
+    </nav> -
+      <Routes>
+        {/* <Route path='/' element={<Home />}/> */}
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+    <footer>프로젝트 입니다.</footer>
+    </div>
+  );
+
+}
+
+function Login() {
+  return(
+    <div>
+      <div>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+      <Button>One</Button>
+      <Button>Two</Button>
+      <Button>Three</Button>
+     </ButtonGroup>
+      </div>
+    </div>
+  );
+}
+
+export default App; 
