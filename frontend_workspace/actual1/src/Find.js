7a771e6b57a7258c27f651d20e2c9715a7d8811a
Null2648 <jhkim208@naver.com>
2023-01-27 16:53:32 +0900

    df

--- frontend_workspace/actual1/src/Find.js
+++ frontend_workspace/actual1/src/Find.js
@@ -197,6 +197,7 @@ export function Products(){
       {data1 && data1.manufacturer}<br/>
       {data1 && data1.period}<br/>
       {data1 && data1.etc}<br/>
+      {Player}
       </div>
     </>
   )
@@ -207,5 +208,50 @@ const Search = () => {
     id: ''
   });
 }
+
+
+
+
+
+
+const useAudio = url => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+      playing ? audio.play() : audio.pause();
+    },
+    [playing]
+  );
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const Player = ({ url }) => {
+  const [playing, toggle] = useAudio(url);
+
+  return (
+    <div>
+      <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+    </div>
+  );
+};
+
+
+
+
+
+
+
+
 
 export default Find;
